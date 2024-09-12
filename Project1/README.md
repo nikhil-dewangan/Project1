a3f7bb8733826ac2b7a450f8b67b9d6e26adba19
Nikhil Dewangan <niktd2767@gmail.com>
2024-09-13 00:00:29 +0530

    Intial commit

--- Project1/README.md
+++ Project1/README.md
@@ -1,3 +1,20 @@
 # Namaste React 🚀🚀🚀
 
 # Project-1🔢
+
+# Parcel
+- Dev Build
+- local server
+- HMR - Hot Module Reload
+- file Watching Algorithm - written in c++
+- caching - faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent
+- code splitting
+- Diffrential Bundling - support older browser
+- diagonist
+
+# Browserslist
--- Project1/app.js
+++ Project1/app.js
@@ -1,21 +1,39 @@
-import React, {Fragment} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id: "parent"},[
+const Header = () => {
+    return (
+        <div className = "header">
+            
+            
+            <div className="logo-container">
+                <img className = "logo" src="https://pics.craiyon.com/2023-05-30/af89e757b5b44b9e9888858b98817801.webp"/>
+            </div>
+            
+            <div className ="nav-item">
+                <ul>
+                    <li>Home </li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+         </div>
+    );
+};
 
-]);
 
 
 
-
-
-
-
-
-
-console.log(parent);
+const AppLayout = () => {
+    return (
+        <div className = "app">
+            <Header/>
+        </div>
+    )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render();
+root.render(<AppLayout/>);
